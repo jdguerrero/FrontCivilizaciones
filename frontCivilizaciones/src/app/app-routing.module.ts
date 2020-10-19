5bced4339e25dffda4945c758cd326673c7c79de
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-10-19 15:58:16 -0500

    Home Component

--- frontCivilizaciones/src/app/app-routing.module.ts
+++ frontCivilizaciones/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/**
+ * import componentes
+ */
+ import { HomeComponent } from './components/home/home.component'
+ import { CivilizacionesComponent } from './components/civilizaciones/civilizaciones.component'
+
+
+
+
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "civilizaciones", component: CivilizacionesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontCivilizaciones/src/app/app.module.ts
+++ frontCivilizaciones/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { HomeComponent } from './components/home/home.component';
+import { CivilizacionesComponent } from './components/civilizaciones/civilizaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegacionComponent
+    NavegacionComponent,
+    HomeComponent,
+    CivilizacionesComponent
   ],
   imports: [
     BrowserModule,
