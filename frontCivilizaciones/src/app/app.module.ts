7ec722589df2e47c70c9de1f402c80f8d2a29396
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-10-19 13:56:31 -0500

    Responsive Navbar

--- frontCivilizaciones/src/app/app.module.ts
+++ frontCivilizaciones/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
