6487aaec933764139f031f10488ab941c51a36b2
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-11-09 12:50:57 -0500

    complete merge ? D:

--- frontCivilizaciones/src/app/app.module.ts
+++ frontCivilizaciones/src/app/app.module.ts
@@ -20,12 +20,7 @@ import { CivilizacionComponent } from './components/civilizaciones/civilizacion/
  */
 
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< Updated upstream
 import { EstilosVidaComponent } from './components/civilizaciones/civilizacion/estilos-vida/estilos-vida.component';
-=======
-import { ReligionesComponent } from './components/civilizaciones/civilizaciones/religiones/religiones.component';
-import { EstiloViadComponent } from './components/civilizaciones/civilizacion/estilo-viad/estilo-viad.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -37,12 +32,7 @@ import { EstiloViadComponent } from './components/civilizaciones/civilizacion/es
     EvaluacionesComponent,
     CivilizacionPreviewComponent,
     CivilizacionComponent,
-<<<<<<< Updated upstream
     EstilosVidaComponent
-=======
-    ReligionesComponent,
-    EstiloViadComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
