4964093eaa1bac5f39d32c29306375b0add970b1
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-11-22 18:59:51 -0500

    update registroTemporal

--- frontCivilizaciones/src/app/components/civilizaciones/civilizacion/civilizacion.component.ts
+++ frontCivilizaciones/src/app/components/civilizaciones/civilizacion/civilizacion.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CivilizacionesService } from '../../../services/civilizaciones.service';
+import {RegistrosTemporalesService } from '../../../services/registrostemporales.service';
 import { ActivatedRoute } from '@angular/router';
 import { Civilizacion } from '../../../models/civilizacion.model';
+import { Registro } from '../../../models/registro.model';
 
 @Component({
   selector: 'app-civilizacion',
@@ -11,16 +13,23 @@ import { Civilizacion } from '../../../models/civilizacion.model';
 export class CivilizacionComponent implements OnInit {
 
   civilizacion: Civilizacion;
+
+  registro: Registro;
+
   id : number;
+
+  idRegistro: number;
   
   
   constructor(
     private ruta:ActivatedRoute,
-
-    private civilizacionesService: CivilizacionesService
-  ) {
+    private civilizacionesService: CivilizacionesService,
+    private registrosTemporalesService: RegistrosTemporalesService
+    ) {
   
     this.civilizacion = new Civilizacion();
+
+    this.registro = new Registro();
     this.ruta.params.subscribe(params=>{
       console.log(params['id'])
 
@@ -36,6 +45,19 @@ export class CivilizacionComponent implements OnInit {
     this.civilizacionesService.getCivilizacionById(this.id).subscribe(Response=>{
 
       this.civilizacion =  Response;
+
+      this.idRegistro = Response.idRegistroTemporal;
+
+      this.registrosTemporalesService.getRegistroTemporalById(this.idRegistro).subscribe(Response=>{
+
+        this.registro.idRegistroTemporal = Response.idRegistroTemporal;
+        this.registro.anioAparicion = Response.añoAparicion;
+        this.registro.anioUltimoRegistro = Response.añoUltimoRegistro;
+        this.registro.descripcion = Response.descripcion;
+
+        console.log(this.registro);
+      });
+
     });
   }
 
