c5778f3cf6fa9a3d85606dbd2d7ecd59b7eb55ab
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-11-23 17:39:40 -0500

    prueba ruta server

--- serve.js
+++ serve.js
@@ -6,6 +6,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/FrontCivilizaciones'));
 
+console.log(__dirname + " soy la ruta");
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/FrontCivilizaciones/index.html'));
