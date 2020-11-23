d2c4ca292f797533ad570602edcceb817342c451
Juan Guerrero <juguerrero9@poligran.edu.co>
2020-11-23 17:20:00 -0500

    update fot deploy 2.0

--- src/serve.js
+++ src/serve.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontcivilizaciones'));
+app.use(express.static(__dirname + '/dist/FrontCivilizaciones'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/frontcivilizaciones/index.html'));
+res.sendFile(path.join(__dirname+'/dist/FrontCivilizaciones/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
